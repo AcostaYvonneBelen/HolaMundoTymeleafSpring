d29aabc27969117f5f8cfefb95997daeff6b5ed0
Yvonne2021 <yvonne_belen_acosta@hotmail.com>
2021-02-18 10:32:35 -0300

    primer commit

---
+++ src/main/java/mx/com/gm/ControladorInicio.java
@@ -0,0 +1,16 @@
+package mx.com.gm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+	
+	@GetMapping("/")
+	public String inicio() {
+		return "index";
+	}
+}
